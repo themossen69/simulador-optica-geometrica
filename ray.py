0c9e6cfbe89eafc65b9a5f97d61d1dc8ff30a19c
themossen69 <ajan@perarnau.cat>
2025-11-25 19:30:36 +0100

    Ray class upgrade

--- ray.py
+++ ray.py
@@ -1,7 +1,30 @@
 import numpy as np
-class Ray:
-    def __init__(self, r: float, theta: float, x_0: float, color: tuple[int, int, int] = (255, 255, 0)):
-        self.xs: np.array[float] = np.array([x_0])
+import matrixs as mx
+from axis_transformation import world_axis_to_screen_axis
+
+class RayState:
+    def __init__(self, r: float, theta: float, x: float = 0.0):
+        self.r: float = r
+        self.theta: float = theta
+        self.x: float = x
+
+    def as_vector(self) -> np.ndarray:
+        return np.array([self.r, self.theta])
+
+class Ray():
+    def __init__(self, r0: float, theta0: float, x_0: float, color: tuple[int, int, int] = (255, 255, 0)):
         self.color: tuple[int, int, int] = color
-        self.path: np.array[float] = np.array([[r, theta]])
+        self.states: list[RayState] = [RayState(r0, theta0, x_0)]
+
+    def aply_matrix(self, M: np.ndarray, d: float):
+        last_state = self.states[-1]
+        new_vector = M @ last_state.as_vector()
+        new_x = last_state.x + d
+        new_state = RayState(new_vector[0], new_vector[1], new_x)
+        self.states.append(new_state)
+    
+    def get_path(self) -> list[tuple[float, float]]:
+        return [(s.x, s.r) for s in self.states]
+
+
 
